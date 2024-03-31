c3a1d6b778e76b3a011a058491e8cc5100edea9d
hemendrasahu67 <57944144+hemendrasahu67@users.noreply.github.com>
2024-03-31 17:52:55 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "29953693"
-api_hash = "bc080dadf0a0e8ca544ac842bcfcf2e0"
-bot_token = "7182994277:AAGz3vbjIweEe1qxENkorYiwf99Lk3aXSOA"
+api_id = "25298674"
+api_hash = "1a7e01278b2bf0fe975fb8ce7c5ead8d"
+bot_token = "7092396095:AAF_jP7qZx_cC0esduKkPafHGt9vhsrWxiM"
